dc3910a8cfa292bb1c6bf4cc7305e12710d6a922
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-11-09 08:52:40 +0000

    Edit require_auth method to determine whether a path is required or not

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,8 +13,22 @@ class Auth:
       current_user
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        '''Returns false'''
-        return False
+        '''returns True if the path is not
+        in the list of strings excluded_paths:
+        Returns True if path is None
+        Returns True if excluded_paths is None or empty
+        Returns False if path is in excluded_paths
+        You can assume excluded_paths contains string path always ending by a /
+        This method must be slash tolerant: path=/api/v1/status
+        and path=/api/v1/status/ must be returned False if
+        excluded_paths contains /api/v1/status/'''
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        else:
+            for paths in excluded_paths:
+                if path == paths or path + '/' == paths:
+                    return False
+            return True
 
     def authorization_header(self, request=None) -> str:
         """Returns None"""
