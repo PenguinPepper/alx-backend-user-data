ede67e4db89d74dead977e6d720e46f61b237f81
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-11-26 11:55:34 +0000

    Create BasicAuth class and implement it to be assigned in app.py

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module contains the class BasicAuth"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Class inherits from Auth
+    """
+    pass
+
